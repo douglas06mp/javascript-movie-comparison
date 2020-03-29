e4474582e5fe8c706d741dd52197e163eb336853
douglas <douglas06mp@gmail.com>
2020-03-29 19:12:10 +0800

    add search preview

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const { data } = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '46fcbba6',
       s: searchTerm
     }
   });
-  console.log(response);
+
+  if (data.Error) return [];
+  return data.Search;
 };
 
-const onInput = e => {
-  fetchData(e.target.value);
+const onInput = async e => {
+  const movies = await fetchData(e.target.value);
+  movies.forEach(movie => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <img src='${movie.Poster}' />
+        <h1>${movie.Title}</h1>
+      `;
+    document.querySelector('.search-preview').append(div);
+  });
 };
 
 const input = document.querySelector('input');
