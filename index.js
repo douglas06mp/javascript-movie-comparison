c9d6a1de77daf88b47cb9d0c8d87dae2194171a9
douglas <douglas06mp@gmail.com>
2020-03-30 12:44:48 +0800

    render movie detail on select

--- index.js
+++ index.js
@@ -25,11 +25,67 @@ const onInput = async e => {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     link.classList.add('dropdown-item');
     link.innerHTML = `
-        <img src='${imgSrc}' />
-        ${movie.Title}
-      `;
-    results.append(link);
+      <img src='${imgSrc}' />
+      ${movie.Title}
+    `;
+    link.addEventListener('click', e => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
+
+    results.appendChild(link);
   });
+};
+
+const onMovieSelect = async movie => {
+  const { data } = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '46fcbba6',
+      i: movie.imdbID
+    }
+  });
+  document.querySelector('.summary').innerHTML = movieTemplate(data);
+};
+
+const movieTemplate = movieDetail => {
+  console.log(movieDetail);
+  return `
+    <article class="media">
+      <figure class=media-left">
+        <p class="image">
+        <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
 };
 
 const autocomplete = document.querySelector('.autocomplete');
